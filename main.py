123a1f7957d8437b6b6830862de53a84cef22ec6
Mateus-Lacerda <mateus.lacerda@discente.ufg.br>
2023-10-28 16:48:15 -0300

    bug fix in agenda data recording

--- main.py
+++ main.py
@@ -4,15 +4,9 @@ import pandas as pd
 
 def main():
     courtTest = court.Court("Basquete", "São Paulo", "100", "1 1 1 1 1 1 1 1 1 1", "1 1 1 1 1 1 1 1 1 1")
-    a = int(input())
-
-    match a:
-        case 1:
-            print(courtTest.getDetails())
-            print(courtTest.courtID)
-            courtTest.bookCourt("João", 1, 3, 5)
-            print(courtTest.checkAvailability(3, 4, 6))
-            print(courtTest.agenda.getAgenda(courtTest.courtID)) #### FALTA CORRIGIR PROBLEMA NA GRAVAÇÃO DE DADOS NA AGENDA
+    courtTest.bookCourt("___________João___________", 1, 3, 5)
+    print(courtTest.checkAvailability(3, 4, 6))
+    print(courtTest.agenda.getAgenda(courtTest.courtID)) #### FALTA CORRIGIR PROBLEMA NA GRAVAÇÃO DE DADOS NA AGENDA
 
 
 if __name__ == '__main__':
