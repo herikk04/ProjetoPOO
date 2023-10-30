b2129fdd9c323bfac4529266353441f043cfe597
Mateus Lacerda <131780349+Mateus-Lacerda@users.noreply.github.com>
2023-10-30 15:45:42 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -1,31 +1,46 @@
 import court
-
+from numpy import zeros
 class Agenda:
     courtAgendaData = []
     
     
     def __init__(self, aCourt, weekDays, weekend):
         self.agendaID = aCourt
-        wD, wE = [(weekDays).split(' ')]*5,[(weekend).split(' ')]*2
-        w = wD + wE
-        self.courtAgenda = w*100
-        for i in range(len(self.courtAgenda)):
-            for j, timeSlot in list(enumerate(self.courtAgenda[i])):
-                if timeSlot == '0':
-                    self.courtAgenda[i][j] = [None, False]
-                elif timeSlot == '1':
-                    self.courtAgenda[i][j] = [None, True]
+        wD, wE = (weekDays).split(' '),(weekend).split(' ')
+        
+        for i, timeSlot in list(enumerate(wD)):
+            if timeSlot == '0':
+                wD[i] = [None, False]
+            elif timeSlot == '1':
+                wD[i] = [None, True]
+        
+        for i, timeSlot in list(enumerate(wE)):
+            if timeSlot == '0':
+                wE[i] = [None, False]
+            elif timeSlot == '1':
+                wE[i] = [None, True]
+     
+        self.courtAgenda = list(zeros(700))
+        for i in range(0,699,7):
+            self.courtAgenda[i] = wD.copy()
+            self.courtAgenda[i+1] = wD.copy()
+            self.courtAgenda[i+2] = wD.copy()
+            self.courtAgenda[i+3] = wD.copy()
+            self.courtAgenda[i+4] = wD.copy()
+            self.courtAgenda[i+5] = wE.copy()
+            self.courtAgenda[i+6] = wE.copy()
+
         self.__class__.courtAgendaData.append(self.courtAgenda)
 
 
     @classmethod
-    def getAgenda(cls, agendaID):
+    def getAgenda(__class__, agendaID):
 
-        return cls.courtAgendaData[agendaID]
+        return __class__.courtAgendaData[agendaID]
     
 
     @classmethod
-    def updateAgenda(cls, agendaID, date, startTime, endTime, value):
+    def updateAgenda(__class__, agendaID, date, startTime, endTime, value):
         for i in range(startTime,endTime+1):
-            cls.courtAgendaData[agendaID][date][i] = value
+            __class__.courtAgendaData[agendaID][date-1][i] = value
         
