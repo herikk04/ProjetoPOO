4a338f732683db50a6ca1c6225f2ab6f931fb45b
Mateus Lacerda Alves Silva <mateussilva@pacto.goiania>
2023-10-31 11:56:14 -0300

    development of add_courts debbug

--- flaskrun.py
+++ flaskrun.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, session
-import user
+from flask import Flask, render_template, request, redirect, session, url_for, Response
+import user, json
 
 app = Flask(__name__)
 app.secret_key = 'sua_chave_secreta'  # Defina uma chave secreta para a sessão
@@ -46,71 +46,98 @@ def add_user_locator():
         phone_number = request.form['phoneNumber']
         ownedCourtsNum = int(request.form['ownedCourtsNum'])
 
-        user.Locator(name, username, password, email, phone_number)
+        thisUser = user.Locator(name, email, phone_number, username, password)
+        userID = thisUser.getSelfUserID()
+        userType = thisUser.getSelfType()
+        print(thisUser.__dict__)
+        print(user.User.userData)
+        print(user.User.getUserObject(userType, userID))
 
-        for _ in range(ownedCourtsNum):
-            return render_template('add_courts.html')
+        return redirect(url_for('add_courts', userID=userID, userType=userType, ownedCourtsNum=ownedCourtsNum))
 
     return render_template('add_user_locator.html')
 
 
 @app.route('/add_courts', methods=['GET', 'POST'])
-def add_courts(): ## PRECISA CONSERTAR REQUESTS DOS HORÁRIOS --> DAR REQUEST NO 'value'
-    if request.method == 'POST':
+def add_courts(): ## PARAMETROS DE ENTRADA: userType, userID não estão sendo recebidos corretamente
+    if request.method == 'GET':
+        ownedCourtsNum = request.args.get('ownedCourtsNum')
+        userID = request.args.get('userID')
+        userType = request.args.get('userType')
+        ownedCourtsNum = int(ownedCourtsNum) if ownedCourtsNum is not None else 0
+
+        return render_template('add_courts.html', userID=userID, userType=userType, ownedCourtsNum=ownedCourtsNum)
+    elif request.method == 'POST':
+        ownedCourtsNum = int(request.args.get('ownedCourtsNum', 0))
+        userID = request.args.get('userID')
+        userType = request.args.get('userType')
+        print(userType,userID)
         courtType = request.form['courtType'] 
         location = request.form['location'] 
         pricePerHour = request.form['pricePerHour']
-        hour_0 = int(request.form.get("hour-0"))
-        hour_1 = int(request.form.get("hour-1"))
-        hour_2 = int(request.form.get("hour-2"))
-        hour_3 = int(request.form.get("hour-3"))
-        hour_4 = int(request.form.get("hour-4"))
-        hour_5 = int(request.form.get("hour-5"))
-        hour_6 = int(request.form.get("hour-6"))
-        hour_7 = int(request.form.get("hour-7"))
-        hour_8 = int(request.form.get("hour-8"))
-        hour_9 = int(request.form.get("hour-9"))
-        hour_10 = int(request.form.get("hour-10"))
-        hour_11 = int(request.form.get("hour-11"))
-        hour_12 = int(request.form.get("hour-12"))
-        hour_13 = int(request.form.get("hour-13"))
-        hour_14 = int(request.form.get("hour-14"))
-        hour_15 = int(request.form.get("hour-15"))
-        hour_16 = int(request.form.get("hour-16"))
-        hour_17 = int(request.form.get("hour-17"))
-        hour_18 = int(request.form.get("hour-18"))
-        hour_19 = int(request.form.get("hour-19"))
-        hour_20 = int(request.form.get("hour-20"))
-        hour_21 = int(request.form.get("hour-21"))
-        hour_22 = int(request.form.get("hour-22"))
-        hour_23 = int(request.form.get("hour-23"))
-        we_hour_0 = int(request.form.get("we_hour-0"))
-        we_hour_1 = int(request.form.get("we_hour-1"))
-        we_hour_2 = int(request.form.get("we_hour-2"))
-        we_hour_3 = int(request.form.get("we_hour-3"))
-        we_hour_4 = int(request.form.get("we_hour-4"))
-        we_hour_5 = int(request.form.get("we_hour-5"))
-        we_hour_6 = int(request.form.get("we_hour-6"))
-        we_hour_7 = int(request.form.get("we_hour-7"))
-        we_hour_8 = int(request.form.get("we_hour-8"))
-        we_hour_9 = int(request.form.get("we_hour-9"))
-        we_hour_10 = int(request.form.get("we_hour-10"))
-        we_hour_11 = int(request.form.get("we_hour-11"))
-        we_hour_12 = int(request.form.get("we_hour-12"))
-        we_hour_13 = int(request.form.get("we_hour-13"))
-        we_hour_14 = int(request.form.get("we_hour-14"))
-        we_hour_15 = int(request.form.get("we_hour-15"))
-        we_hour_16 = int(request.form.get("we_hour-16"))
-        we_hour_17 = int(request.form.get("we_hour-17"))
-        we_hour_18 = int(request.form.get("we_hour-18"))
-        we_hour_19 = int(request.form.get("we_hour-19"))
-        we_hour_20 = int(request.form.get("we_hour-20"))
-        we_hour_21 = int(request.form.get("we_hour-21"))
-        we_hour_22 = int(request.form.get("we_hour-22"))
-        we_hour_23 = int(request.form.get("we_hour-23"))
+        hour_0 = int(request.form.get("hour-0", 0))
+        hour_1 = int(request.form.get("hour-1", 0))
+        hour_2 = int(request.form.get("hour-2", 0))
+        hour_3 = int(request.form.get("hour-3", 0))
+        hour_4 = int(request.form.get("hour-4", 0))
+        hour_5 = int(request.form.get("hour-5", 0))
+        hour_6 = int(request.form.get("hour-6", 0))
+        hour_7 = int(request.form.get("hour-7", 0))
+        hour_8 = int(request.form.get("hour-8", 0))
+        hour_9 = int(request.form.get("hour-9", 0))
+        hour_10 = int(request.form.get("hour-10", 0))
+        hour_11 = int(request.form.get("hour-11", 0))
+        hour_12 = int(request.form.get("hour-12", 0))
+        hour_13 = int(request.form.get("hour-13", 0))
+        hour_14 = int(request.form.get("hour-14", 0))
+        hour_15 = int(request.form.get("hour-15", 0))
+        hour_16 = int(request.form.get("hour-16", 0))
+        hour_17 = int(request.form.get("hour-17", 0))
+        hour_18 = int(request.form.get("hour-18", 0))
+        hour_19 = int(request.form.get("hour-19", 0))
+        hour_20 = int(request.form.get("hour-20", 0))
+        hour_21 = int(request.form.get("hour-21", 0))
+        hour_22 = int(request.form.get("hour-22", 0))
+        hour_23 = int(request.form.get("hour-23", 0))
+        we_hour_0 = int(request.form.get("we_hour-0", 0))
+        we_hour_1 = int(request.form.get("we_hour-1", 0))
+        we_hour_2 = int(request.form.get("we_hour-2", 0))
+        we_hour_3 = int(request.form.get("we_hour-3", 0))
+        we_hour_4 = int(request.form.get("we_hour-4", 0))
+        we_hour_5 = int(request.form.get("we_hour-5", 0))
+        we_hour_6 = int(request.form.get("we_hour-6", 0))
+        we_hour_7 = int(request.form.get("we_hour-7", 0))
+        we_hour_8 = int(request.form.get("we_hour-8", 0))
+        we_hour_9 = int(request.form.get("we_hour-9", 0))
+        we_hour_10 = int(request.form.get("we_hour-10", 0))
+        we_hour_11 = int(request.form.get("we_hour-11", 0))
+        we_hour_12 = int(request.form.get("we_hour-12", 0))
+        we_hour_13 = int(request.form.get("we_hour-13", 0))
+        we_hour_14 = int(request.form.get("we_hour-14", 0))
+        we_hour_15 = int(request.form.get("we_hour-15", 0))
+        we_hour_16 = int(request.form.get("we_hour-16", 0))
+        we_hour_17 = int(request.form.get("we_hour-17", 0))
+        we_hour_18 = int(request.form.get("we_hour-18", 0))
+        we_hour_19 = int(request.form.get("we_hour-19", 0))
+        we_hour_20 = int(request.form.get("we_hour-20", 0))
+        we_hour_21 = int(request.form.get("we_hour-21", 0))
+        we_hour_22 = int(request.form.get("we_hour-22", 0))
+        we_hour_23 = int(request.form.get("we_hour-23", 0))
         weedDays = [hour_0, hour_1, hour_2, hour_3, hour_4, hour_5, hour_6, hour_7, hour_8, hour_9, hour_10, hour_11, hour_12, hour_13, hour_14, hour_15, hour_16, hour_17, hour_18, hour_19, hour_20, hour_21, hour_22, hour_23]
         weekend = [we_hour_0, we_hour_1, we_hour_2, we_hour_3, we_hour_4, we_hour_5, we_hour_6, we_hour_7, we_hour_8, we_hour_9, we_hour_10, we_hour_11, we_hour_12, we_hour_13, we_hour_14, we_hour_15, we_hour_16, we_hour_17, we_hour_18, we_hour_19, we_hour_20, we_hour_21, we_hour_22, we_hour_23]
-    return render_template('add_courts.html')
+        
+        print(user.User.getUserObject(userType, userID))
+        thisUser = user.User.getUserObject(userType, userID)
+        print(thisUser)
+        thisUser.addCourts(courtType, location, pricePerHour, weekend, weedDays)
+    
+        return render_template('add_courts.html', userID=userID, userType=userType, ownedCourtsNum=ownedCourtsNum)
+    else:
+        # Handle the case where thisUser is None, possibly by redirecting or showing an error message
+        return "Invalid request."
+
+        
+
 
 @app.route('/dashboard')
 def dashboard():
@@ -124,6 +151,7 @@ def dashboard():
 def logout():
     session.pop('username', None)
     session.pop('user_type', None)
+    
     return redirect('/')
 
 
--- user.py
+++ user.py
@@ -3,23 +3,47 @@ class User:
     ser = 0
     userData = {"Locator": [], "Renter": []}
 
-    def __init__(self, name, email, phoneNumber):
+    def __init__(self, name, email, phoneNumber, username, password):
+        self.userType = None
         self.userID = self.__class__.ser
         self.__class__.ser+=1
         self.name = name
         self.email = email
         self.phoneNumber = phoneNumber
+        self.username = username
+        self.password = password
 
+
+    def getSelfType(self):
+            
+        return self.userType
+
+
+    def getSelfUserID(self):
+
+        return self.userID
+
+
+    @classmethod
+    def getUserObject(__class__, userType, userID):
+        if userType == "Locator":
+
+            return __class__.userData["Locator"][0][0]["object"]
+        elif userType == "Renter":
+
+            return __class__.userData["Renter"][0][0]["object"]
 
 class Locator(User):
     ser = 0
 
-    def __init__(self, name, username, password, email, phoneNumber): ## TERMINAR A LÓGICA DE SENHA E USUÁRIO
-        super().__init__(name, email, phoneNumber)
+    def __init__(self, name, email, phoneNumber, username, password): ## TERMINAR A LÓGICA DE SENHA E USUÁRIO
+        super(Locator,self).__init__(name, email, phoneNumber, username, password)
+        self.userType = "Locator"
         self.locatorID = self.__class__.ser
         self.__class__.ser+=1
         self.ownedCourts = []
-        super().userData["Locator"].append([self.locatorID, self.name, self.email, self.phoneNumber, self.ownedCourts]) ## parâmetro self.ownedCourts tem os ID's das quadras
+        self.object = self
+        super().userData["Locator"].append([self.__dict__]) ## parâmetro self.ownedCourts tem os ID's das quadras
 
 
     def addCourts(self, courtType, location, pricePerHour, weekend, week_days):
@@ -29,22 +53,23 @@ class Locator(User):
 class Renter(User):
     ser = 0
 
-    def __init__(self, name, username, password, email, phoneNumber):
-        super().__init__(name, email, phoneNumber)
+    def __init__(self, name, email, phoneNumber, username, password,):
+        super().__init__(name, email, phoneNumber, username, password)
+        self.userType = "Renter"
         self.renterID = self.__class__.ser
         self.__class__.ser+=1
         self.reservations = []
-        super().userData["Renter"].append(self.renterID, self.name, self.email, self.phoneNumber, self.reservations)
+        super().userData["Renter"].append([self, self.userType, self.userID, self.renterID, self.username, self.password, self.name, self.email, self.phoneNumber, self.reservations])
 
 
     @classmethod
     def registerReservation(__class__, reservationID, userID):
-        super().userData["Renter"][userID][4].append(reservationID)
+        super().userData["Renter"][0][userID][8].append(reservationID)
 
 
     @classmethod
     def unregisterReservation(__class__, reservationID, userID):
-        del super().userData["Renter"][userID][4][reservationID] ## PRECISA SER OBSERVADO --> NA HORA DE REMOVER A RESERVA CERTA
+        del super().userData["Renter"][0][userID][8][reservationID] ## PRECISA SER OBSERVADO --> NA HORA DE REMOVER A RESERVA CERTA
     
     
     @classmethod
